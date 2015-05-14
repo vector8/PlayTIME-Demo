4d1673d7087199ecf8394a6dfe6350593d656e27
vector8 <vector8@localhost>
2015-05-14 13:53:28 -0400

    First commit.

---
+++ PlayTIME UI Mockup/Assets/Scripts/UIControlScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIControlScript : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void goToNextScreen(GameObject nextScreen)
+	{
+		nextScreen.SetActive (true);
+		gameObject.SetActive (false);
+	}
+
+	public void instantiateScreen(GameObject prefab)
+	{
+		GameObject screenClone = (GameObject)Instantiate (prefab);
+		screenClone.SetActive (true);
+		gameObject.SetActive (false);
+	}
+
+	public void destroySelf()
+	{
+		Destroy (gameObject);
+	}
+}
