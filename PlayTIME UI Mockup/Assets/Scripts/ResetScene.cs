8b75e634939661495f6d3ef8d176e16252499afb
vector8 <vector8@localhost>
2015-05-19 16:42:46 -0400

    Added a way to reset the station back to its beginning state. Fixed anchoring and spacing issues for UI elements so they behave properly when window is resized.

---
+++ PlayTIME UI Mockup/Assets/Scripts/ResetScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetScene : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		if(Event.current.Equals(Event.KeyboardEvent("r")))
+		{
+			Application.LoadLevel("ActionStation");
+		}
+	}
+}
