80cff963481c16647bfbc1488f12048132d26c00
vector8 <vector8@localhost>
2015-05-20 17:11:17 -0400

    Set up basis for extending the game between two displays. Need to add actual scene functionality, currently is just splitting one view between two monitors.

--- 2DProjectTest/Assets/Scripts/LevelDesignTIME.cs
+++ 2DProjectTest/Assets/Scripts/LevelDesignTIME.cs
@@ -13,6 +13,9 @@ public class LevelDesignTIME : MonoBehaviour
     public GameObject MouseModeActiveText;
     public Vector2 PlacementUIOffsetInPixels;
 
+	public Vector2 topScreenResolution;
+	public Vector2 bottomScreenResolution;
+
     private Vector3 LastMousePosition;
 
     // Reference to the board (Grid)
@@ -32,6 +35,10 @@ public class LevelDesignTIME : MonoBehaviour
 	// Use this for initialization
 	void Start () 
     {
+		Screen.SetResolution((int)topScreenResolution.x + (int)bottomScreenResolution.x - 1, 
+		                     (int)topScreenResolution.y + (int)bottomScreenResolution.y - 1, 
+		                     false);
+
 	    // Add things to the data base
         // Key will be replaced with rfid values
         for (int i = 0; i < prefabs.Length; i++)
