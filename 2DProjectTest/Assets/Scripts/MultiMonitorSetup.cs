b4ac7d4f02614fb9976b83a351dd720fe543e8f2
vector8 <vector8@localhost>
2015-05-21 13:28:22 -0400

    Modified to resize and reposition the camera based on the dimensions of the two monitors. This covers the case where the monitors are of differing dimensions. Dimensions of the two monitors can be set on the MainCamera object.

---
+++ 2DProjectTest/Assets/Scripts/MultiMonitorSetup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiMonitorSetup : MonoBehaviour 
+{
+	public Vector2 topScreenResolution;
+	public Vector2 bottomScreenResolution;
+
+	private Camera cam;
+	private Vector3 pos;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		Screen.SetResolution((int)Mathf.Min(topScreenResolution.x, bottomScreenResolution.x), 
+		                     (int)topScreenResolution.y + (int)bottomScreenResolution.y, 
+		                     false);
+
+		cam = GetComponent<Camera>();
+		if(bottomScreenResolution.y > 0)
+		{
+			cam.orthographicSize = 5 * (topScreenResolution.y / bottomScreenResolution.y + 1f);
+		}
+		pos = gameObject.transform.position;
+		pos.y = -1.5f + cam.orthographicSize;
+		gameObject.transform.position = pos;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
