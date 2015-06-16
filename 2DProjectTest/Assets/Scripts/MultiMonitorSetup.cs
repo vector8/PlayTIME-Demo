b04a4514e795c3b9e4751ea4f2ab1bba8b941280
vector8 <vector8@users.noreply.github.com>
2015-06-16 11:26:55 -0400

    Fixed multi-monitor camera issues.

--- 2DProjectTest/Assets/Scripts/MultiMonitorSetup.cs
+++ 2DProjectTest/Assets/Scripts/MultiMonitorSetup.cs
@@ -5,25 +5,37 @@ public class MultiMonitorSetup : MonoBehaviour
 {
 	public Vector2 topScreenResolution;
 	public Vector2 bottomScreenResolution;
-
-	private Camera cam;
+	public Camera topCam;
+	
+	private Camera bottomCam;
 	private Vector3 pos;
 	
 	// Use this for initialization
 	void Start () 
 	{
+		float totalYResolution = topScreenResolution.y + bottomScreenResolution.y;
 		Screen.SetResolution((int)Mathf.Min(topScreenResolution.x, bottomScreenResolution.x), 
-		                     (int)topScreenResolution.y + (int)bottomScreenResolution.y, 
+		                     (int)totalYResolution, 
 		                     false);
+		
+		bottomCam = GetComponent<Camera>();
 
-		cam = GetComponent<Camera>();
-		if(bottomScreenResolution.y > 0)
-		{
-			cam.orthographicSize = 5 * (topScreenResolution.y / bottomScreenResolution.y + 1f);
-		}
-		pos = gameObject.transform.position;
-		pos.y = -1.5f + cam.orthographicSize;
-		gameObject.transform.position = pos;
+		Rect rect = bottomCam.rect;
+		rect.height = bottomScreenResolution.y / totalYResolution;
+		bottomCam.rect = rect;
+
+		rect = topCam.rect;
+		rect.height = topScreenResolution.y / totalYResolution;
+		rect.y = 1f - rect.height;
+		topCam.rect = rect;
+
+//		if(bottomScreenResolution.y > 0)
+//		{
+//			cam.orthographicSize = 5 * (topScreenResolution.y / bottomScreenResolution.y + 1f);
+//		}
+//		pos = gameObject.transform.position;
+//		pos.y = -1.5f + cam.orthographicSize;
+//		gameObject.transform.position = pos;
 	}
 	
 	// Update is called once per frame
